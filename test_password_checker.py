ca16827956ce174d47785341ab552bb24f929377
Aventinis <125674720+Aventinis@users.noreply.github.com>
2026-09-11 11:27:38 +0200

    Added Whiteboxtest

--- test_password_checker.py
+++ test_password_checker.py
@@ -9,4 +9,15 @@ def test_password_checker():
     assert is_valid_password("12345678") == False
     assert is_valid_password("!@#$%^&*()") == False
     assert is_valid_password("ValidPass1!") == True
-    
+
+
+def test_password_length():
+    assert is_valid_password("Short1!") == False
+    assert is_valid_password("LongEnough1!") == True
+
+def test_password_character_requirements():
+    assert is_valid_password("NoNumbers!") == False
+    assert is_valid_password("nouppercase1!") == False
+    assert is_valid_password("NOLOWERCASE1!") == False
+    assert is_valid_password("NoSpecialChar1") == False
+    assert is_valid_password("ValidPass1!") == True
